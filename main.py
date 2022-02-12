4eeb55e1eac63dc4e00ac26081cc538d0ea1a280
GreenDevth <theerapong@rumail.ru.ac.th>
2022-02-12 15:05:20 +0700

    Edit bot status

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from lib2to3.pgen2 import token
 import discord
 from discord.ext import commands
 from discord_components import DiscordComponents
 from config.auth import get_token, config_cogs
 
 intents = discord.Intents.default()
-intents.members=True
-bot = commands.Bot(command_prefix='$' , intents=intents)
+intents.members = True
+bot = commands.Bot(command_prefix='$', intents=intents)
 DiscordComponents(bot)
 token = get_token(8)
+
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
-
+    await bot.change_presence(
+        status=discord.Status.online,
+        activity=discord.Activity(type=discord.ActivityType.watching, name='Discord Store.')
+    )
 
 
 config_cogs(bot)
