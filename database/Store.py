643e46d433b81f7303ea57df5f0f00c458fdc12f
GreenDevth <theerapong@rumail.ru.ac.th>
2022-02-12 08:46:44 +0000

    add get package price

--- database/Store.py
+++ database/Store.py
@@ -16,3 +16,16 @@ def get_command(package_name):
             return res[0]
     except Error as e:
         print(e)
+
+def get_prict(package_name):
+    try:
+        conn = MySQLConnection(**db)
+        cur = conn.cursor()
+        sql = 'SELECT package_price FROM scum_package WHERE package_name = %s'
+        cur.execute(sql, (package_name,))
+        row = cur.fetchone()
+        while row is not None:
+            res = list(row)
+            return res[0]
+    except Error as e:
+        print(e)
