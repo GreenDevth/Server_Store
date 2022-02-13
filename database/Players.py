c85e6a99874cd79dd6aec7d6e001478354e55123
GreenDevth <theerapong@rumail.ru.ac.th>
2022-02-13 13:37:55 +0700

    test server status button

--- database/Players.py
+++ database/Players.py
@@ -5,10 +5,11 @@ db = read_db_config()
 
 
 def players_exists(discord_id):
+    """ Check register players """
     try:
         conn = MySQLConnection(**db)
         cur = conn.cursor()
-        sql = 'SELECT COUNT(STEAM_ID) FROM scum_players WHERE DISCORD_ID = %s'
+        sql = 'SELECT COUNT(*) FROM scum_players WHERE DISCORD_ID = %s'
         cur.execute(sql, (discord_id,))
         row = cur.fetchone()
         while row is not None:
