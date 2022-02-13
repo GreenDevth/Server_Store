6aeb52cf832e1cbf2a4d3b546b997ec0828e716a
GreenDevth <theerapong@rumail.ru.ac.th>
2022-02-13 08:01:50 +0700

    edit store

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from config.auth import get_token, config_cogs
 
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 DiscordComponents(bot)
 token = get_token(8)
 
